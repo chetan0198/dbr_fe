f63d6b28d38371b00e1bcdd1e00aee3da5cd0082
chetansp011@gmail.com <chetansp011@gmail.com>
2026-04-23 14:22:31 +0000

    demo pyspark code
    code to read and transform with pyspark

--- second_file_py.py
+++ second_file_py.py
@@ -1,2 +1,53 @@
 # Databricks notebook source
 # This is a .py file.
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+
+# COMMAND ----------
+
+# DBTITLE 1,Create Data (list → DataFrame)
+data = [
+    (1, "Alice", 25),
+    (2, "Bob", 30),
+    (3, "Chet", 28)
+]
+
+columns = ["id", "name", "age"]
+
+df = spark.createDataFrame(data, columns)
+df.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Basic Transformations
+
+# COMMAND ----------
+
+# DBTITLE 1,Filter
+df.filter(df.age > 26).show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Select specific columns
+df.select("name", "age").show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Add new column
+from pyspark.sql.functions import col
+
+df = df.withColumn("age_plus_5", col("age") + 5)
+df.show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Aggregation
+df.groupBy().avg("age").show()
+
+# COMMAND ----------
+
+# DBTITLE 1,Sorting
+df.orderBy("age").show()
